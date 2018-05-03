8baa0831e76328f766d46c8aa0ab26f11619080f
Mark Watkin <Mark_Watkin@eafsupplychain.com>
2018-05-03 07:11:17 +0100

    Tidying up

--- src/lib.hat/backlight.c
+++ src/lib.hat/backlight.c
@@ -17,79 +17,67 @@
 #define NOT_OPEN(dot3k) (dot3k->backlight_i2c_fd <= 0)
 
 static uint8_t clamp_u8(int val) {
-    if(val < 0) return 0;
-    if(val > 255) return 255;
-    return (uint8_t) val;
+	if(val < 0) return 0;
+	if(val > 255) return 255;
+	return (uint8_t) val;
 }
-
-/*
-void disco(int fd) {
-    uint8_t x[18];
-    for(int i = 0; i < 18; i++) x[i] = rand() & 1 ? 255 : 0;
-    i2c_smbus_write_i2c_block_data(fd, CMD_SET_PWM_VALUES, 18, x);
-}
-
-void set_all_leds(int fd, int value) {
-    uint8_t x[18];
-    memset(x, value, 18);
-    i2c_smbus_write_i2c_block_data(fd, CMD_SET_PWM_VALUES, 18, x);
-}
-*/
-
 
 static void dot3k_bl_update(DOT3K *dot3k) {
-    if(NOT_OPEN(dot3k)) return;
-    i2c_smbus_write_i2c_block_data(dot3k->backlight_i2c_fd, CMD_UPDATE, 1, (uint8_t[]){0x01});
+	if(NOT_OPEN(dot3k)) return;
+	ioctl(fd, I2C_SLAVE, 0x54);
+	i2c_smbus_write_i2c_block_data(dot3k->backlight_i2c_fd, CMD_UPDATE, 1, (uint8_t[]){0x01});
 }
 
 void dot3k_bl_reset(DOT3K *dot3k) {
-    if(NOT_OPEN(dot3k)) return;
-    i2c_smbus_write_i2c_block_data(dot3k->backlight_i2c_fd, CMD_RESET, 1, (uint8_t[]){0xFF});
+	if(NOT_OPEN(dot3k)) return;
+	ioctl(fd, I2C_SLAVE, 0x54);
+	i2c_smbus_write_i2c_block_data(dot3k->backlight_i2c_fd, CMD_RESET, 1, (uint8_t[]){0xFF});
 }
 
 void dot3k_bl_enable(DOT3K *dot3k, int enable) {
-    if(NOT_OPEN(dot3k)) return;
-    i2c_smbus_write_i2c_block_data(dot3k->backlight_i2c_fd, CMD_ENABLE_OUTPUT, 1, (uint8_t[]){enable ? 0x01 : 0x00});
+	if(NOT_OPEN(dot3k)) return;
+	ioctl(fd, I2C_SLAVE, 0x54);
+	i2c_smbus_write_i2c_block_data(dot3k->backlight_i2c_fd, CMD_ENABLE_OUTPUT, 1, (uint8_t[]){enable ? 0x01 : 0x00});
 }
 
 void dot3k_bl_enable_leds(DOT3K *dot3k, uint32_t leds) {
-    if(NOT_OPEN(dot3k)) return;
-    dot3k->backlight_enable = leds;
-    uint8_t data[3] = {
-        leds & 0xFF,
-        (leds >> 8) & 0xFF,
-        (leds >> 16) & 0xFF
-    };
-    i2c_smbus_write_i2c_block_data(dot3k->backlight_i2c_fd, CMD_ENABLE_LEDS, 3, data);
+	if(NOT_OPEN(dot3k)) return;
+	dot3k->backlight_enable = leds;
+	uint8_t data[3] = {
+		leds & 0xFF,
+		(leds >> 8) & 0xFF,
+		(leds >> 16) & 0xFF
+	};
+	i2c_smbus_write_i2c_block_data(dot3k->backlight_i2c_fd, CMD_ENABLE_LEDS, 3, data);
 }
 
 void dot3k_bl_update_brightnesses(DOT3K *dot3k) {
-    if(NOT_OPEN(dot3k)) return;
-    uint8_t data[18]={0};
-    for(int led = 0; led < 18; led++) {
-        uint8_t level = dot3k->backlight_level[led];
-        data[led] = dot3k->backlight_level_map[led][level];
-        //printf("%02d: %02x %02x\n", led, level, data[led]);
-    }
-    i2c_smbus_write_i2c_block_data(dot3k->backlight_i2c_fd, CMD_SET_PWM_VALUES, 18, data);
-    dot3k_bl_update(dot3k);
+	if(NOT_OPEN(dot3k)) return;
+	uint8_t data[18]={0};
+	for(int led = 0; led < 18; led++) {
+		uint8_t level = dot3k->backlight_level[led];
+		data[led] = dot3k->backlight_level_map[led][level];
+	}
+	ioctl(fd, I2C_SLAVE, 0x54);
+	i2c_smbus_write_i2c_block_data(dot3k->backlight_i2c_fd, CMD_SET_PWM_VALUES, 18, data);
+	dot3k_bl_update(dot3k);
 }
 
 void dot3k_bl_set_brightnesses(DOT3K *dot3k, int offset, int count, uint8_t brightnesses[]) {
-    if(NOT_OPEN(dot3k)) return;
-    for(int i = offset; i < count; i++) {
-        if(i >= 18) break;
-        dot3k->backlight_level[i] = *brightnesses;
-        brightnesses++;
-    }
+	if(NOT_OPEN(dot3k)) return;
+	for(int i = offset; i < count; i++) {
+		if(i >= 18) break;
+		dot3k->backlight_level[i] = *brightnesses;
+		brightnesses++;
+	}
 }
 
 void dot3k_bl_set_brightness(DOT3K *dot3k, int offset, int count, uint8_t brightness) {
-    if(NOT_OPEN(dot3k)) return;
-    for(int i = offset; i < count; i++) {
-        if(i >= 18) break;
-        dot3k->backlight_level[i] = brightness;
-    }
+	if(NOT_OPEN(dot3k)) return;
+	for(int i = offset; i < count; i++) {
+		if(i >= 18) break;
+		dot3k->backlight_level[i] = brightness;
+	}
 }
 
 void dot3k_bl_set_screen_rgb(DOT3K *dot3k, int8_t pos, uint8_t r, uint8_t g, uint8_t b) {
@@ -108,65 +96,65 @@ void dot3k_bl_set_screen_rgb(DOT3K *dot3k, int8_t pos, uint8_t r, uint8_t g, uin
 }
 
 void dot3k_bl_set_bar_graph(DOT3K *dot3k, float value, uint8_t brightness) {
-    if(NOT_OPEN(dot3k)) return;
-    for(int i = 0; i < 9; i++) {
-        float p = i / 9.0f;
-        uint8_t dst = (value >= p ? brightness : 0);
-        dot3k->backlight_level[9 + i] = dst;
-    }
+	if(NOT_OPEN(dot3k)) return;
+	for(int i = 0; i < 9; i++) {
+		float p = i / 9.0f;
+		uint8_t dst = (value >= p ? brightness : 0);
+		dot3k->backlight_level[9 + i] = dst;
+	}
 }
 
 void dot3k_bl_set_bar_graph_train(DOT3K *dot3k, uint8_t brightness_on, uint8_t brightness_off, int position, int wrap) {
-    if(NOT_OPEN(dot3k)) return;
-    if(position < 0) return;
-    if(wrap == 1) {
-        position %= 18;
-        if(position >= 9) position = 17 - position;
-    } else {
-        position %= 9;
-    }
-    uint8_t train[9] = {0};
-    memset(train, brightness_off, 9);
-    train[position] = brightness_on;
-    memcpy(dot3k->backlight_level + 9, train, 9);
+	if(NOT_OPEN(dot3k)) return;
+	if(position < 0) return;
+	if(wrap == 1) {
+		position %= 18;
+		if(position >= 9) position = 17 - position;
+	} else {
+		position %= 9;
+	}
+	uint8_t train[9] = {0};
+	memset(train, brightness_off, 9);
+	train[position] = brightness_on;
+	memcpy(dot3k->backlight_level + 9, train, 9);
 }
 
 void dot3k_bl_close(DOT3K *dot3k) {
-    if(NOT_OPEN(dot3k)) return;
-    dot3k_bl_enable(dot3k, 0);
-    close(dot3k->backlight_i2c_fd);
-    dot3k->backlight_i2c_fd = 0;
+	if(NOT_OPEN(dot3k)) return;
+	dot3k_bl_enable(dot3k, 0);
+	close(dot3k->backlight_i2c_fd);
+	dot3k->backlight_i2c_fd = 0;
 }
 
 int dot3k_bl_open(DOT3K *dot3k) {
-    dot3k_bl_close(dot3k);
-    int fd = open("/dev/i2c-1", O_RDWR);
-    if(fd < 0) {
-        perror("opening i2c for backlight");
-        return 1;
-    }
-    dot3k->backlight_i2c_fd = fd;
-    ioctl(fd, I2C_SLAVE, 0x54);
-    dot3k_bl_reset(dot3k);
-    for(int led = 0; led < 18; led++) {
-        for(int level = 0; level < 256; level++) {
-            dot3k->backlight_level_map[led][level] = level;
-        }
-    }
-    return 0;
+	dot3k_bl_close(dot3k);
+	int fd = open("/dev/i2c-1", O_RDWR);
+	if(fd < 0) {
+		perror("opening i2c for backlight");
+		return 1;
+	}
+	dot3k->backlight_i2c_fd = fd;
+	ioctl(fd, I2C_SLAVE, 0x54);
+	dot3k_bl_reset(dot3k);
+	for(int led = 0; led < 18; led++) {
+		for(int level = 0; level < 256; level++) {
+			dot3k->backlight_level_map[led][level] = level;
+		}
+	}
+	return 0;
 }
 
 void dot3k_bl_calibrate(DOT3K *dot3k, float gamma, float r_div, float g_div, float b_div) {
-    for(int level = 0; level < 256; level++) {
-        float p = powf(level / 256.0f, gamma);
-        for(int off = 0; off < 3; off++) {
-            uint8_t r = clamp_u8(p / r_div * 255.0f);
-            uint8_t g = clamp_u8(p / g_div * 255.0f);
-            uint8_t b = clamp_u8(p / b_div * 255.0f);
-            dot3k->backlight_level_map[off * 3 + 0][level] = clamp_u8(r);
-            dot3k->backlight_level_map[off * 3 + 1][level] = clamp_u8(g);
-            dot3k->backlight_level_map[off * 3 + 2][level] = clamp_u8(b);
-            //printf("%02x: %02x %02x %02x\n", level, r, g, b);
-        }
-    }
+	for(int level = 0; level < 256; level++) {
+		float p = powf(level / 256.0f, gamma);
+		for(int off = 0; off < 3; off++) {
+			uint8_t r = clamp_u8(p / r_div * 255.0f);
+			uint8_t g = clamp_u8(p / g_div * 255.0f);
+			uint8_t b = clamp_u8(p / b_div * 255.0f);
+			dot3k->backlight_level_map[off * 3 + 0][level] = clamp_u8(r);
+			dot3k->backlight_level_map[off * 3 + 1][level] = clamp_u8(g);
+			dot3k->backlight_level_map[off * 3 + 2][level] = clamp_u8(b);
+			//printf("%02x: %02x %02x %02x\n", level, r, g, b);
+		}
+	}
 }
